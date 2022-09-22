802b24c0dbe487baf8c81364f92e860bda5b2f79
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2022-09-22 10:40:44 +0530

    Add files via upload

---
+++ primenumb.py
@@ -0,0 +1,15 @@
+# num = int(input())
+# if num > 1:
+#  for i in range(2, int(num/2)+1):
+#   if (num % i) == 0:
+#    print(num, "is not a prime number")
+#    break
+#  else:
+#   print(num, "is a prime number")
+# else:
+#  print(num, "is not a prime number")
+a = 20
+b = 20
+if a>b:
+    print("a is ")
+    print("b")    
